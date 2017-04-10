ff1d4d9fbf288d7cd01eda1056182a46575a523d
sedoyinside <sedoyinside@yandex.ru>
2017-04-10 15:31:37 +0300

    hgjhj

--- src/test/UnusedInterface.java
+++ src/test/UnusedInterface.java
@@ -4,4 +4,7 @@ package com.aurea.deadcode.test;
  * Created by sedoy on 06.04.17.
  */
 public interface UnusedInterface {
+    public static final Strin TEST_UNUSED_STATIC_PUB ="dfdfs";
+    private static final Strin TEST_UNUSED_STATIC_PRIV ="3433";
+
 }
